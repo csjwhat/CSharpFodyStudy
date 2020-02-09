376de463d8a690fea91a7f9ee69ba842112af908
csjwhats <csjwhats@gmail.com>
2020-02-09 21:19:08 +0900

    一応プロパティ変更のサンプルもコミット

---
+++ BasicSpa/AutoLogger/OnPropertySetAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Cauldron.Interception;
+using System;
+
+namespace CauldronInterceptionFodyExample.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class OnPropertySetAttribute : Attribute, IPropertySetterInterceptor
+    {
+        [AssignMethod("{CtorArgument:0}")]
+        public Action<string, object> onSetMethod = null;
+
+        public OnPropertySetAttribute(string methodName)
+        {
+        }
+
+        public void OnException(Exception e)
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+
+        public bool OnSet(PropertyInterceptionInfo propertyInterceptionInfo, object oldValue, object newValue)
+        {
+            this.onSetMethod?.Invoke(propertyInterceptionInfo.PropertyName, newValue);
+            return false;
+        }
+    }
+}
